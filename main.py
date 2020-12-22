f6a942414937687b734d0fe7da343e5335558350
Verardi Donato, B2B-BPN-BAN-PDC-FRO-BPM-BPD <Donato.Verardi@swisscom.com>
2020-12-22 15:07:36 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,71 @@
+# link: https://www.youtube.com/watch?v=FfWpgLFMI7w
+import pygame
+import random
+
+# initialize the pygame
+pygame.init()
+
+# create the screen
+screen = pygame.display.set_mode((800, 600))
+
+#Title and icon
+# link to download icons: https://www.flaticon.com/search?word=spaceship
+
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+# Player
+# https://www.flaticon.com/search?word=arcade
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+# enemy
+# https://www.flaticon.com/search?word=alien
+enemyImg = pygame.image.load('enemy.png')
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 150)
+enemyX_change = 0
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
+
+
+# Game loop
+running = True
+while running:
+    # red green blue
+    screen.fill((0, 0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        # if any keystroke is being pressed
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        # if any keystroke is being released
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
+                playerX_change = 0
+
+    playerX += playerX_change
+
+    # boundaries
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    player(playerX, playerY)
+    enemy(enemyX, enemyY)
+    pygame.display.update()
